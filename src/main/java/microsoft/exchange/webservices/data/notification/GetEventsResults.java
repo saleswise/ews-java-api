a35f9f6ef93e6eb82a536df9063cf86344ede065
Yiyang Song <songyiyang@Yiyangs-MacBook-Pro-2.local>
2016-04-07 16:21:48 -0400

    Change the access of function `getNewWatermark` from protected to public

--- src/main/java/microsoft/exchange/webservices/data/notification/GetEventsResults.java
+++ src/main/java/microsoft/exchange/webservices/data/notification/GetEventsResults.java
@@ -201,7 +201,7 @@ public final class GetEventsResults {
    *
    * @return the new watermark
    */
-  protected String getNewWatermark() {
+  public String getNewWatermark() {
     return newWatermark;
   }
 
